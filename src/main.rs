0eed2012628bfa03fdce588502fe972441bc5e98
Pedro Lopez <pdihax@gmail.com>
2024-07-09 19:27:50 -0300

    Add basic axum server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use axum::routing::get;
+use tracing::info;
+use tracing_subscriber::FmtSubscriber;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let _ = tracing::subscriber::set_global_default(FmtSubscriber::default());
+    
+    let app = axum::Router::new().route("/", get(| | async { "Hello, World! "}));
+
+    info!("Starting server");
+
+    axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await?;
+    Ok(())
 }
